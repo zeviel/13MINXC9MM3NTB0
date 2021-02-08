bbc9912ce4bb32f16ddd131001247c82e839e29d
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-02-08 12:19:58 +0800

    Add files via upload

---
+++ AminoCommentBo.py
@@ -0,0 +1,38 @@
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+print(Back.BLACK)
+print(Fore.RED)
+print("Script by Zevi/Скрипт сделан Zevi")
+print("┌────────────────────────────────────┐")
+print("│Author :  LilZevi                   │")
+print("│Github : https://github.com/LilZevi │")
+print("└────────────────────────────────────┘")
+print("YouTube: https://www.youtube.com/channel/UCJ61JlXJckmO6yJr8BDRuGQ")
+print("▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄")
+print("█▀█ █░█░█ █ █░▀█ █░█")
+print("▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░")
+print("▄▀ ▄▀▄ █▄░▄█ █▄░▄█ █▀▀ █▄░█ ▀█▀")
+print("█░ █░█ █░█░█ █░█░█ █▀▀ █░▀█ ░█░")
+print("░▀ ░▀░ ▀░░░▀ ▀░░░▀ ▀▀▀ ▀░░▀ ░▀░")
+print("█▀▄ ▄▀▄ ▀█▀")
+print("█▀█ █░█ ░█░")
+print("▀▀░ ░▀░ ░▀░")
+import amino
+#инпут почты и пароля для входа в аккаунт
+email=input("Email/Почта:")
+password=input("Password/Пароль:")
+client = amino.Client()
+client.login(email=email, password=password)
+print('\nLogged in/Бот зашел!')
+bloglink=input("Blog Link/Ссылка на Блог:")
+blog=client.get_from_code(bloglink)
+blogId=blog.objectId
+comId=blog.path[1:blog.path.index('/')]
+message=input("Message/Сообщение:")
+sub_client = amino.SubClient(comId=comId,profile=client.profile)
+while True:
+	try:
+		sub_client.comment(message=message,blogId=blogId)
+		print("Comment Sended/Комментарий отправлен!")
+	except: pass
