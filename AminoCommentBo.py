321e90d1c460bf9eae897e92113663e49a5e35b3
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-06-10 14:13:27 +0600

    Update AminoCommentBo.py

--- AminoCommentBo.py
+++ AminoCommentBo.py
@@ -1,33 +1,25 @@
-from colorama import init
-from colorama import Fore, Back, Style
+import amino
+import pyfiglet
+from colorama import init, Fore, Back, Style
 init()
-print(Back.BLACK)
 print(Fore.RED)
 print("""Script by Lil Zevi
-Github : https://github.com/LilZevi
-▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄
-█▀█ █░█░█ █ █░▀█ █░█
-▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░
-▄▀ ▄▀▄ █▄░▄█ █▄░▄█ █▀▀ █▄░█ ▀█▀
-█░ █░█ █░█░█ █░█░█ █▀▀ █░▀█ ░█░
-░▀ ░▀░ ▀░░░▀ ▀░░░▀ ▀▀▀ ▀░░▀ ░▀░
-█▀▄ ▄▀▄ ▀█▀
-█▀█ █░█ ░█░
-▀▀░ ░▀░ ░▀░""")
-import amino
-email=input("Email/Почта:")
-password=input("Password/Пароль:")
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("amino", font="ogre"))
+print(pyfiglet.figlet_format("commentbo", font="big"))
 client = amino.Client()
+email=input("Email/Почта: ")
+password=input("Password/Пароль: ")
 client.login(email=email, password=password)
-print('\nLogged in/Бот зашел!')
-bloglink = input("Blog Link/Ссылка на Блог:")
+bloglink = input("Blog Link/Ссылка на Блог: ")
 blog = client.get_from_code(bloglink)
 blogId = blog.objectId
 comId = blog.path[1:blog.path.index('/')]
-message = input("Message/Сообщение:")
+message = input("Message/Сообщение: ")
 sub_client = amino.SubClient(comId=comId,profile=client.profile)
 while True:
 	try:
 		sub_client.comment(message=message,blogId=blogId)
 		print("Comment Sended/Комментарий отправлен!")
-	except: pass
+	except:
+		pass
