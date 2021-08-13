e2886d6e18711bf7d92a719e14f7cfb6bc220591
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-08-14 01:22:57 +0600

    Update AminoCommentBo.py

--- AminoCommentBo.py
+++ AminoCommentBo.py
@@ -1,4 +1,5 @@
 import amino
+import asyncio
 import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
@@ -7,19 +8,23 @@ print("""Script by Lil Zevi
 Github : https://github.com/LilZevi""")
 print(pyfiglet.figlet_format("amino", font="ogre"))
 print(pyfiglet.figlet_format("commentbo", font="big"))
-client = amino.Client()
-email=input("Email/Почта: ")
-password=input("Password/Пароль: ")
-client.login(email=email, password=password)
-bloglink = input("Blog Link/Ссылка на Блог: ")
-blog = client.get_from_code(bloglink)
-blogId = blog.objectId
-comId = blog.path[1:blog.path.index('/')]
-message = input("Message/Сообщение: ")
-sub_client = amino.SubClient(comId=comId,profile=client.profile)
-while True:
-	try:
-		sub_client.comment(message=message,blogId=blogId)
-		print("Comment Sended/Комментарий отправлен!")
-	except:
-		pass
+
+async def main():
+	client = amino.Client()
+	email = input("Email >> ")
+	password = input("Password >> ")
+	await client.login(email=email, password=password)
+	bloglink = input("Blog Link >> ")
+	blog_info = await client.get_from_code(bloglink)
+	blogId = blog_info.objectId
+	communityid = blog_info.path[1:blog_info.path.index('/')]
+	message = input("Message >> ")
+	sub_client = await amino.SubClient(comId=communityid, profile=client.profile)
+	while True:
+		try:
+			await sub_client.comment(message=message, blogId=blogId)
+			print("Comment Sended")
+		except:
+			pass
+
+asyncio.get_event_loop().run_until_complete(main())
